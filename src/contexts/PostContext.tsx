9e82163178609ffdd3e66696a2e70046fc0b05ce
conejofoz <conejofoz@gmail.com>
2025-01-02 11:36:01 -0300

    persistindo context em localstorage

--- src/contexts/PostContext.tsx
+++ src/contexts/PostContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, Dispatch, ReactNode, useContext, useReducer } from "react";
+import { createContext, Dispatch, ReactNode, useContext, useEffect, useReducer } from "react";
 import { Post } from "../types/Post";
 import { PostActions, postReducer } from "../reducers/postReducer";
+
+const STORAGE_KEY = 'postContextContent';
 
 type PostContextType = {
     posts: Post[];
@@ -8,8 +10,16 @@ type PostContextType = {
 }
 export const PostContext = createContext<PostContextType | null >(null);
 
+
 export const PostProvider = ({children}: {children: ReactNode})=>{
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(
+        postReducer, 
+        JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    );
+
+    useEffect(()=>{
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts));
+    }, [posts]);
 
     return (
         <PostContext.Provider value={{ posts, dispatch }}>
